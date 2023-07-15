c96de2468bd719e659f3ede40e550b99f365909a
ad <adhiraj2004@icloud.com>
2023-07-15 11:32:41 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+// Endpoints to serve the html
 app.get('/', (req, res) => {
   res.sendFile("pages/index.html",{root: __dirname})
 })
@@ -9,6 +9,27 @@ app.get('/login', (req, res) => {
     res.sendFile("pages/login.html",{root: __dirname})
 })
 app.get('/signup', (req, res) => {
+    res.sendFile("pages/signup.html",{root: __dirname})
+})
+//endpoints for APIs
+app.post('/getnotes', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("pages/signup.html",{root: __dirname})
+})
+app.post('/login', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("pages/signup.html",{root: __dirname})
+})
+app.post('/signup', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("pages/signup.html",{root: __dirname})
+})
+app.post('/addnote', (req, res) => {
+    const {userToken} = req.body
+    res.sendFile("pages/signup.html",{root: __dirname})
+})
+app.post('/deletenode', (req, res) => {
+    const {userToken} = req.body
     res.sendFile("pages/signup.html",{root: __dirname})
 })
 app.listen(port, () => {
